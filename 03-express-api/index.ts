890a76c82028a4e2f0a699f9cb71e29b2dbf37cc
unicoast <237638569+unicoast@users.noreply.github.com>
2026-04-30 01:06:56 -0400

    add: ejemplo proyecto express con TS

---
+++ 03-express-api/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(201).json({
+        ok: true,
+        msg: 'Nuevo ID: 12345'
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
